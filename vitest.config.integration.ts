afd46ee1d98e6bbf41af2df5cbc69d169a0f6766
Vladimír Volek <volek.vladimir@gmail.com>
2025-06-03 13:09:13 +0200

    fix: folder structure

--- vitest.config.integration.ts
+++ vitest.config.integration.ts
@@ -1,17 +1,14 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
-if (!["mainnet", "preprod", "preview"].includes(process.env.NETWORK || "")) {
-  throw "Error NETWORK env variable can be only `mainnet, preview, preprod, ipfs, milkomeda-mainnet, milkomeda-testnet`";
+if (!['mainnet', 'preprod', 'preview'].includes(process.env.NETWORK || '')) {
+  throw 'Error NETWORK env variable can be only `mainnet, preview, preprod, ipfs, milkomeda-mainnet, milkomeda-testnet`';
 }
 
-const testFolders = [
-  `./test/integration/tests/${process.env.NETWORK}/**/*.ts`,
-  `./test/integration/tests/common/**/*.ts`,
-];
+const testFolders = [`./src/tests/${process.env.NETWORK}/**/*.ts`, `./src/tests/common/**/*.ts`];
 
 export default defineConfig({
   test: {
-    reporters: ["verbose"],
+    reporters: ['verbose'],
     // Be aware that there is also customTimeout option that can be set in fixtures
     // AND DEFAULT_TEST_TIMEOUT ./test/integration/utils
     testTimeout: 15_000,
@@ -21,7 +18,7 @@ export default defineConfig({
     hookTimeout: 20_000,
     include: testFolders,
     passWithNoTests: true,
-    setupFiles: ["./test/integration/setup.ts"],
+    setupFiles: ['./src/setup.ts'],
     chaiConfig: {
       truncateThreshold: 40_000,
     },
