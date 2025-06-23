8146e5a61da699584df1f18f0c7dffa42df4b31f
Vladimír Volek <volek.vladimir@gmail.com>
2025-06-23 14:12:58 +0200

    fix: eslint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,20 @@ import tseslint from 'typescript-eslint';
 import eslintConfigPrettierFlat from 'eslint-config-prettier/flat';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default tseslint.config(
+const globalIgnores = {
+  ignores: [
+    '**/.yarn/**',
+    'node_modules/**',
+    'lib/**',
+    'submodules/**',
+    '.eslintrc.js',
+    'openapi.ts',
+  ],
+};
+
+const srcRules = tseslint.config(
   {
     files: ['src/**/*.{js,ts,tsx}'],
-    ignores: ['**/.yarn/**', 'node_modules/**', 'lib/**', '.eslintrc.js', 'openapi.ts'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
@@ -22,3 +32,5 @@ export default tseslint.config(
     },
   },
 );
+
+export default [globalIgnores, ...srcRules];
