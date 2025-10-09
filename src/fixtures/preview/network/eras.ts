993243f81261733c0273386476d5c3bb56be577b
Vladimír Volek <volek.vladimir@gmail.com>
2025-10-09 13:55:11 +0200

    fix: preview network eras

--- src/fixtures/preview/network/eras.ts
+++ src/fixtures/preview/network/eras.ts
@@ -2,7 +2,6 @@ import { expect } from 'vitest';
 
 export default [
   {
-    id: 'network-eras_161d67e8a062',
     testName: 'network eras',
     endpoints: ['network/eras'],
     response: [
@@ -33,16 +32,19 @@ export default [
       },
       {
         start: { time: 259_200, slot: 259_200, epoch: 3 },
-        end: { time: 55_728_000, slot: 55_728_000, epoch: 645 },
+        end: {
+          time: 55814400,
+          slot: 55814400,
+          epoch: 646,
+        },
         parameters: { epoch_length: 86_400, slot_length: 1, safe_zone: 25_920 },
       },
       {
-        start: { time: 55728000, slot: 55728000, epoch: 645 },
-        end: { time: 55814400, slot: 55814400, epoch: 646 },
-        parameters: { epoch_length: 86400, slot_length: 1, safe_zone: 25920 },
-      },
-      {
-        start: { time: 55814400, slot: 55814400, epoch: 646 },
+        start: {
+          time: 55814400,
+          slot: 55814400,
+          epoch: 646,
+        },
         end: {
           time: expect.toBeUnixTimestamp(),
           slot: expect.toBeGreaterThan(55_728_000),
