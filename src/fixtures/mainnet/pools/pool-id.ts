f72895a6bbca3c19b5f4310a2fb8a23e17e325a4
Vladimír Volek <volek.vladimir@gmail.com>
2025-10-10 16:00:27 +0200

    fix: pools registrations

--- src/fixtures/mainnet/pools/pool-id.ts
+++ src/fixtures/mainnet/pools/pool-id.ts
@@ -421,15 +421,7 @@ export default [
       fixed_cost: expect.toBePositive(),
       reward_account: expect.toBeStakeAddress(),
       owners: ['stake1uxlf7lctg849r0nkunzmp3rdmtpvktm5yj4d4vjn6sd7gdg57vdcv'],
-      registration: [
-        'e3d9917cbd8c377e5f1cbeba189040a4e3f360472f0d9496604c59be320758ae',
-        'd369911773416826f80c5ff4459c3d25b923e269307e0f4ec6400c38ac132ee9',
-        '498b4aaa4497c64cf1a84669bf2e2242fea20c585b77dad000df0080183122b8',
-        '9e47f9f4075829985bc25c329a40afdfd390fe52554938b17aa282594f52ae0c',
-        '055058f15d72c75d4baf3245c56210f75c7431e0caf9aca559e6784cbb740763',
-        '9f3947f3b7ad863c1cde8068a249f0db56c2ae7f69f30f5b77329ef37674af55',
-        '16f8b7fd58781c6fa37960ed37f2a246ad2675884d708d187dec5b4f3c30f4ce',
-      ],
+      registration: expect.arrayContaining([expect.any(String)]),
       retirement: [],
       calidus_key: {
         nonce: 152255188,
