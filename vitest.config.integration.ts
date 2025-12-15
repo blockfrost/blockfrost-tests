d02a2da735ebefa2aa1d7be96b505072203efcaa
Vladimír Volek <volek.vladimir@gmail.com>
2025-12-15 16:05:32 +0100

    fix: reporter

--- vitest.config.integration.ts
+++ vitest.config.integration.ts
@@ -8,7 +8,7 @@ const testFolders = [`./src/tests/${process.env.NETWORK}/**/*.ts`, `./src/tests/
 
 export default defineConfig({
   test: {
-    reporters: process.env.GITHUB_ACTIONS ? ['dot', 'github-actions'] : ['verbose'],
+    reporters: process.env.GITHUB_ACTIONS ? ['github-actions'] : ['verbose'],
     // Be aware that there is also customTimeout option that can be set in fixtures
     // AND DEFAULT_TEST_TIMEOUT ./test/integration/utils
     testTimeout: 15_000,
