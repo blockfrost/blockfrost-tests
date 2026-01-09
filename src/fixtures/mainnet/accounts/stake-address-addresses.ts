058fba8af9504b1df429453b69b00c98758993e5
slowbackspace <slowbackspace@gmail.com>
2026-01-09 10:42:57 +0100

    chore: accounts/:sa/addresses tests with many addresses

--- src/fixtures/mainnet/accounts/stake-address-addresses.ts
+++ src/fixtures/mainnet/accounts/stake-address-addresses.ts
@@ -11,6 +11,28 @@ export default [
     ],
   },
   {
+    testName: 'accounts/:stake_address bf stake address with 4 addresses',
+    endpoints: ['accounts/stake1u9uz4j024qfud557ucrqw3kqfdndjgaxj7m44x7tamkvmyqzdwe7v/addresses'],
+    response: [
+      {
+        address:
+          'addr1q8zsjx7vxkl4esfejafhxthyew8c54c9ch95gkv3nz37sxrc9ty742qncmffaesxqarvqjmxmy36d9aht2duhmhvekgq3jd3w2',
+      },
+      {
+        address:
+          'addr1qxrrzqsqnzqx3p8sxxsry936h6c78ml4rdl224f33l7pmcnc9ty742qncmffaesxqarvqjmxmy36d9aht2duhmhvekgqr735lq',
+      },
+      {
+        address:
+          'addr1q9x625ny9y42s5z8n78afjg9meyeknvt5kwm3y6sdlrz66tc9ty742qncmffaesxqarvqjmxmy36d9aht2duhmhvekgqsyx3uz',
+      },
+      {
+        address:
+          'addr1qy6qvd3szupa7ayqf6zw7cd0ple7w3yg5f3xh5gkkc4q9zmc9ty742qncmffaesxqarvqjmxmy36d9aht2duhmhvekgq52e2en',
+      },
+    ],
+  },
+  {
     id: 'accounts-stake-address-generic-stake-address-with-zero-addresses_58a5c07d3316',
     testName: 'accounts/:stake_address generic stake address with zero addresses',
     endpoints: ['accounts/stake1u9q7y25vy0l4jan6k8r0g2zn2ranezmmk0jnprvvmxqns9cvnhhkl/addresses'],
